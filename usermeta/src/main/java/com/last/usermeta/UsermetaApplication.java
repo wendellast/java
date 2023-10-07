c8e8db382ac161c7120755d8a5148ae1439b1757
wendellast <wendelalves01a@hotmail.com>
2023-10-07 17:55:53 -0300

    ap+

---
+++ usermeta/src/main/java/com/last/usermeta/UsermetaApplication.java
@@ -0,0 +1,13 @@
+package com.last.usermeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermetaApplication.class, args);
+	}
+
+}
---
+++ usermeta/src/test/java/com/last/usermeta/UsermetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.last.usermeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
